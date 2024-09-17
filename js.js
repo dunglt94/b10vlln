c0423ecab8b33f18276379115528a27e999f5a2f
dunglt94 <tuandung9594@gmail.com>
2024-09-17 14:11:28 +0700

    them vong lap while

--- js.js
+++ js.js
@@ -1,11 +1,24 @@
-let sout;
-sout = "<table border='1' width='300' cellspacing='0' cellpadding='0'>";
-for (let i = 1; i <= 10; i++) {
+let sout, i, j;
+sout = "<table border='1' width='300' cellspacing='0' cellpadding='3'>";
+// for (let i = 1; i <= 10; i++) {
+//     sout += "<tr>";
+//     for (let j = 1; j <= 10; j++) {
+//         sout += "<td width='30' style='text-align: center'>" + i * j + "</td>";
+//     }
+//     sout += "</tr>";
+// }
+// sout += "</table>";
+// document.write(sout);
+i = j = 1;
+while ( i<= 10) {
     sout += "<tr>";
-    for (let j = 1; j < 10; j++) {
+    while (j <= 10) {
         sout += "<td width='30' style='text-align: center'>" + i * j + "</td>";
+        j++;
     }
     sout += "</tr>";
+    j = 1;
+    i++;
 }
 sout += "</table>";
 document.write(sout);
